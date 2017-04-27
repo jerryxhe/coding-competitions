f48b5b125ea07b2b1cc089709dbf8bb9cf66dad1
jerryxhe <rebound1618@gmail.com>
2017-04-27 09:12:41 -0400

    Update stepping_stones.py

--- stepping_stones.py
+++ stepping_stones.py
@@ -6,7 +6,7 @@ T = int(raw_input())
 for _ in xrange(T):
     n = int(raw_input())
     k=int(sqrt(2*n))
-    if k*(k+1) != n:
+    if k*(k+1) != 2*n:
         print "Better Luck Next Time"
     else:
-        print "Go On Bob "+str(k+1)
+        print "Go On Bob "+str(k)
