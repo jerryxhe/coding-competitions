6c78a26085b0e0da922160b28c16327176c347d8
jerryxhe <rebound1618@gmail.com>
2017-04-27 08:56:51 -0400

    Create stepping_stones.py

---
+++ stepping_stones.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/stepping-stones-game
+
+from math import sqrt
+T = int(raw_input())
+
+for _ in xrange(T):
+    n = int(raw_input())
+    k=int(sqrt(2*n))
+    if k*(k+1) != n:
+        print "Better Luck Next Time"
+    else:
+        print "Go On Bob "+str(k)
